4441b7aa9198df218a8181c087f00eed9cf7e341
Roman Savrulin <savrulin@flexibity.com>
2016-06-21 00:20:39 +0300

    basic auth for single client implemented

--- TelegramMotionBot.py
+++ TelegramMotionBot.py
@@ -1,4 +1,5 @@
 import json
+import hashlib
 #telegram
 from telegram.error import NetworkError, Unauthorized
 from telegram.ext import Updater
@@ -21,6 +22,7 @@ class Bot(object):
         self.logger.info( "Initing telegram API..." )
         self.u = Updater(token=conf["token"])
         self.bot = self.u.bot
+        self.passwordHash = conf["password_hash"]
 
         dp = self.u.dispatcher
 
@@ -28,39 +30,74 @@ class Bot(object):
     	dp.add_handler(CommandHandler('stop', self.stopCmd))
     	dp.add_handler(CommandHandler('status', self.statusCmd))
     	dp.add_handler(CommandHandler('frame', self.frameCmd))
+        dp.add_handler(CommandHandler('deauth', self.deauthCmd))
     	dp.add_error_handler(self.error)
 
     	self.chatId = None
+        self.started = False
     	self.u.start_polling()
     	self.logger.info( "Telegram Bot API started" )
         self.sendSingleFrame = False
 
     def startCmd(self, bot, u):
-    	self.chatId = u.message.chat_id
-    	self.bot.sendMessage(chat_id=self.chatId, text="Starting motion detector") #TODO: add authorization phase
+        if self.chatId == None:
+            chat_id = u.message.chat_id
+            arglist = u.message.text.split(" ", 2)
+            if (len(arglist) > 1):
+                m = hashlib.md5()
+                m.update(arglist[1])
+
+                #self.logger.info("Authorization attempt: " + m.hexdigest() + ":" + self.passwordHash)
+
+                if(m.hexdigest() == self.passwordHash):
+                    self.chatId = chat_id
+                    self.started = True
+                    self.bot.sendMessage(chat_id=self.chatId, text="Starting motion detector")
+                else:
+                    self.bot.sendMessage(chat_id=chat_id, text="Password doesn't match ")
+            else:
+                self.bot.sendMessage(chat_id=chat_id, text="Use /start <password> to start motion detector feed to this channel")
+        else:
+            if self.checkAuth(u):
+                self.bot.sendMessage(chat_id=self.chatId, text="Starting motion detector")
+                self.started = True
+
+    def checkAuth(self, u):
+        if self.chatId != None and u.message.chat_id == self.chatId:
+            return True
+        else:
+            self.bot.sendMessage(chat_id=u.message.chat_id, text="Unauthorized!")
+
+    def deauthCmd(self, bot, u):
+        if self.checkAuth(u):
+            self.bot.sendMessage(chat_id=self.chatId, text="Deauthorizing bot...")
+            self.started = False
+            self.chatId = None
 
     def stopCmd(self, bot, u):
-    	self.bot.sendMessage(chat_id=u.message.chat_id, text="Stopping detection")
-    	self.chatId = None
+        if self.checkAuth(u):
+            self.bot.sendMessage(chat_id=self.chatId, text="Stopping detection")
+            self.started = False
 
     def frameCmd(self, bot, u):
-    	self.bot.sendMessage(chat_id=u.message.chat_id, text="Sending current frame")
-    	self.sendSingleFrame = True
+        if self.checkAuth(u):
+            self.bot.sendMessage(chat_id=self.chatId, text="Sending current frame")
+            self.sendSingleFrame = True
 
     def statusCmd(self, bot, u):
-    	if (self.chatId is None):
-    		self.bot.sendMessage(chat_id=u.message.chat_id, text="Motion notifications not enabled")
-    	else:
-    		self.bot.sendMessage(chat_id=u.message.chat_id, text="Sending Motion notifications to " + `self.chatId`)
+        if self.checkAuth(u):
+            if (self.started == False):
+                self.bot.sendMessage(chat_id=self.chatId, text="Motion notifications not enabled")
+            else:
+                self.bot.sendMessage(chat_id=self.chatId, text="Sending Motion notifications to " + `self.chatId`)
 
     def error(self, bot, update, error):
         self.logger.warn('Update "%s" caused error "%s"' % (update, error))
 
     def sendFrame(self, frame):
         chat_id = self.chatId
-        self.sendSingleFrame = False
     	try:
-    		if chat_id != None:
+            if chat_id != None and (self.started or self.sendSingleFrame):
     			self.logger.debug("sending image")
     			t = TempImage()
     			cv2.imwrite(t.path, frame)
@@ -70,10 +107,13 @@ class Bot(object):
     		self.logger.error("network error")
     	except Unauthorized:
     		# The user has removed or blocked the bot.
-    		self.chatId = None
-    		self.logger.error( "Unauth. User has removed bot" )
+            self.chatId = None
+            self.started = False
+            self.logger.error( "Unauth. User has removed bot" )
     	except:
     		self.logger.error( "Unknown" )
+
+        self.sendSingleFrame = False
 
     def stop(self):
        self.logger.info( "stopping telegram" )
