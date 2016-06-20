e7fc1aea6706970c47a25249f038db67b7e56f09
Roman Savrulin <savrulin@flexibity.com>
2016-06-20 23:38:49 +0300

    password generator now correctly handles input string

--- gen-hash.py
+++ gen-hash.py
@@ -7,8 +7,8 @@ stdin = sys.stdin
 #salt = os.urandom(16)
 
 print 'enter password:'
-password = stdin.readline()
+password = stdin.readline().split('\n')[0]
 
 m = hashlib.md5()
 m.update(password)
-print 'hash is: ' + m.hexdigest()
+print 'hash for ' + password + ' (' + `len(password)` + ') is: ' + m.hexdigest()
